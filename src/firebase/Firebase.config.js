79a5ff2d5fe3692d652a23e58b6d791818cbc36d
Shanto57575 <shanto57575@gmail.com>
2023-04-28 16:28:53 +0600

    first commit

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyD_cmXhlVF9Ta6Tk9sReM69untGAhGyHCo",
+    authDomain: "ema-john-with-firebase-a-6f9e0.firebaseapp.com",
+    projectId: "ema-john-with-firebase-a-6f9e0",
+    storageBucket: "ema-john-with-firebase-a-6f9e0.appspot.com",
+    messagingSenderId: "126719967978",
+    appId: "1:126719967978:web:d4e6cf2c7f8afe820c0d93"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
